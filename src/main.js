71b990c7523551709b3f6f66ae0125d76b45062a
1sKaNdRR <151454522+1sKaNdRR@users.noreply.github.com>
2024-05-12 16:49:10 +0200

    js-hw-11-1123

--- src/main.js
+++ src/main.js
@@ -1,36 +1,3 @@
-// import { fetchImages } from './js/pixabay-api.js';
-// import { renderGallery, showErrorMessage, createGalleryItemMarkup } from './js/render-functions.js';
-
-
-
-// const searchForm = document.getElementById('search-form');
-// const searchInput = document.getElementById('search-input');
-
-
-
-
-
-
-
-// searchForm.addEventListener('submit', async (event) => {
-//     event.preventDefault();
-
-//     const searchTerm = searchInput.value.trim();
-//     if (!searchTerm) {
-//         showErrorMessage('Sorry, there are no images matching your search query. Please try again!');
-//         return;
-//     }
-
-//     try {
-//         const images = await fetchImages(searchTerm);
-//         console.log(images)
-//         renderGallery(images);
-//     } catch (error) {
-//         showErrorMessage('Failed to fetch images. Please try again.');
-//     }
-// });
-
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
